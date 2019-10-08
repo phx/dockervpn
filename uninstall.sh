8d0b375db5b1d3a9e51e3d28401e9f5a1d2463e0
phx <oldjamey@gmail.com>
2019-10-08 08:56:01 -0500

    compatibility with old image naming scheme

--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,12 @@ sudo rm -vf /usr/local/bin/dockervpn
 sudo rm -vf /usr/local/bin/sshvpn
 echo -e '\nStopping any running DockerVPN cantainer...'
 sudo docker stop vpn 2>/dev/null
+sudo docker stop dockervpn 2>/dev/null
 echo -e '\nRemoving any stopped DockerVPN containers...'
 sudo docker rm vpn 2>/dev/null
+sudo docker rm dockervpn 2>/dev/null
 echo -e '\nRemoving the DockerVPN image...'
 sudo docker rmi vpn 2>/dev/null
+sudo docker rmi dockervpn 2>/dev/null
 echo -e '\nDone.\n'
 
